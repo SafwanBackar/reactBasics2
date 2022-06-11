929ff386ea6bb4ce6927c6fa8ba3a9a2bcefe664
Safwan Backar <safwanbackar29@gmail.com>
2022-06-11 19:37:07 +0530

    react portals

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import LifecycleA from './Components/LifecycleA';
 import RefsDemo from './Components/RefsDemo';
+import PortalDemo from './Components/PortalDemo';
 
 function App() {
   return (
     <div className="App">
       {/* <LifecycleA/> */}
-      <RefsDemo/>
+      {/* <RefsDemo/> */}
+      {/* Added a new element in index.html */}
+      <PortalDemo/>
     </div>
   );
 }
